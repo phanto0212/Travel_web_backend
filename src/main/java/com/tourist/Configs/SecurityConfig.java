08f5197e7f3026fc52d749615792fd1e0f4a86bd
phanto0212 <phanto02122004@gmail.com>
2025-04-30 13:35:44 +0700

    add new cors

--- src/main/java/com/tourist/Configs/SecurityConfig.java
+++ src/main/java/com/tourist/Configs/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://travel-web-sage.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://travel-web-sage.vercel.app","https://travelwebbackend-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Cho phép cookies và thông tin xác thực
